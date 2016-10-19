7b2374dca8c6248def877cf74d9e9d65d312c479
J. Heasly <jheasly@earthlink.net>
2016-10-18 18:06:09 -0700

    Replicating data structure
    of script, e.g. save_election_lane_setup_20160517.py

--- management/commands/ballot_setup.py
+++ management/commands/ballot_setup.py
@@ -1,13 +1,25 @@
 from ballot.models import Cand_yes_no
 from django.core.management.base import BaseCommand, CommandError
 from xlrd import open_workbook
-import os
 
+import os
+import pprint
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        wb = open_workbook('Lane Results.orig.xlsx')
+        wb = open_workbook('Lane Results.xlsx')
         sheets = wb.sheets()
-        for sheet in sheets:
-            print sheet.name
+        for sheet in wb.sheet_names():
+            contest_number = sheet.split()[-1]
+            # 0859
+            out_list = []
+            out_list.append( wb.sheet_by_name(sheet).cell(6,0).value )
+            cand_meas_names = wb.sheet_by_name(sheet).row_values(6, 2)
+            # [u'Joshua Skov', u'Emily Semple', u'Write-in']
+            # [u'Yes', u'No']
+            for cand_meas_name in cand_meas_names:
+                out_list.append(
+                    (contest_number, cand_meas_name,)
+                )
+            pprint.pprint( out_list )
         print str(len(sheets)).format('{0} races/contests')
