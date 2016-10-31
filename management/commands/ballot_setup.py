355afb91cb3cbaf6b6406f545f1c2209b2caabb9
John Heasly <jheasly@earthlink.net>
2016-10-30 21:49:38 -0700

    New ballot_setup

--- management/commands/ballot_setup.py
+++ management/commands/ballot_setup.py
@@ -1,70 +1,168 @@
-from ballot.models import Cand_yes_no
+from ballot.management.commands.ballot_settings import CSV_FILE_NAMES
+
+from ballot.models import Cand_yes_no, Contest, Contest_wrapper, Region
 from django.core.management.base import BaseCommand, CommandError
 
+import csv
 import os
 import sys
 import time
 
-try:
-    from selenium import webdriver
-except ImportError:
-    sys.exit('This script meant for local use only.')
+HEADER_CUTOFF = 1
+AFFILIATION_LOOKUP = {
+    'CON': 'C',
+    'DEM': 'D',
+    'IND': 'I',
+    'LBT': 'L',
+#     'LIB': 'L',
+#     'PCE': 'P',
+    'PRO': 'P',
+#     'PGN': 'PG',
+    'PGP': 'PG',
+    'REP': 'R',
+    'WFP': 'WF',
+    'NON': 'N',
+}
+FILE_PATH = os.path.realpath(os.path.join(os.path.dirname(__file__)))
 
-SITE_ADDR = 'http://results.oregonvotes.gov/ResultsExport.aspx'
-BROWSER_DOWNLOAD_DIR = '/Users/jheasly/Downloads'
-CSV_FILE_NAME = 'Lane_County_Media_Export.csv'
 
-class ScrapingBrowser(webdriver.Chrome):
-    def __init__(self, addr, *args, **kwargs):
-        super(ScrapingBrowser, self).__init__(*args, **kwargs)
-        self.implicitly_wait(3)
-        self.get(addr)
-
-    def click_county_download(self, location):
-        self.find_element_by_xpath(location).click()
+class GroupBy(dict):
+    def __init__(self, seq, key=lambda x:x):
+        for value in seq:
+            k = key(value)
+            self.setdefault(k, []).append(value)
+    __iter__ = dict.iteritems
 
 
 class Command(BaseCommand):
-    # TODO: Prior to download, check to see if there's already and
-    # Lane_County_Media_Export.csv and if so, DELETE ...
-
     help = '''Run this one-time only per election. It DELETES all the Contest,
  Cand_yes_no and Contest model data from the previous election (It leaves the
   Region data alone, however!). It does a quick-and-dirty creation of the races
   and measures for you.'''
 
     def handle(self, *args, **options):
-        # Selenium replicates browser click to download .csv
-        browser = ScrapingBrowser(SITE_ADDR)
-        time.sleep(3)
-        browser.click_county_download('/html/body/form/div[4]/div[4]/div[3]/div/div[20]/a')
-        time.sleep(10)
-        browser.close()
-        browser.quit()
+        for csv_file_name in CSV_FILE_NAMES:
+            csvfile = open(os.path.join(FILE_PATH, csv_file_name['file']), 'rb')
+            lines = list(csv.reader(csvfile))
 
-        # e.g., /Users/jheasly/Development/ballot/management
-        FILE_PATH = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+        header_lines = lines[0:HEADER_CUTOFF]
+        for header_line in header_lines:
+            print '[Header line]:', header_line, '\n'
 
+        body_lines = lines[HEADER_CUTOFF:]
+        data = []
 
-        # '''
-        # START Excel sheets
-        # '''
-        # wb = open_workbook('Lane Results.xlsx')
-        # sheets = wb.sheets()
-        # for sheet in wb.sheet_names():
-        #     contest_number = sheet.split()[-1]
-        #     # 0859
-        #     out_list = []
-        #     out_list.append( wb.sheet_by_name(sheet).cell(6,0).value )
-        #     cand_meas_names = wb.sheet_by_name(sheet).row_values(6, 2)
-        #     # [u'Joshua Skov', u'Emily Semple', u'Write-in']
-        #     # [u'Yes', u'No']
-        #     for cand_meas_name in cand_meas_names:
-        #         out_list.append(
-        #             (contest_number, cand_meas_name,)
-        #         )
-        #     pprint.pprint( out_list )
-        # print str(len(sheets)).format('{0} races/contests')
-        # '''
-        # END Excel sheets
-        # '''
+        for line in body_lines:
+            data.append((
+            int(line[0]),           # 'ContestID'
+            line[1].strip(),        # 'ContestName'
+            line[2],                # 'PartyCode'
+            int(line[7]),           # 'CandidateID'
+            line[8].strip(),        # 'CandidateName'
+            line[9],                # 'CurrentDateTime'
+            int(line[10]),          # 'VoteFor'
+            int(line[11]),          # 'CandidateVotes'
+            float(line[12]),        # 'CandidatePercentage'
+            line[13],               # 'PrecinctsReporting'
+            ))
+
+        out_list = []
+        record_list = []
+        sub_list = []
+        '''
+        Creates a parent list/sub-list (see examples below) grouped by whatever
+        item 'r[x]' selects in the original "flat" list. The key for the parent list
+        is a duplicate of whatever value r[x] is.
+        '''
+        for k, g in GroupBy(data, key=lambda r: r[0]): # <- this is the field you group on
+            record_list.append(k)
+            for record in g:
+                sub_list.append(record)
+            record_list.append(sub_list)
+            sub_list = []
+            out_list.append(record_list)
+            record_list = []
+
+        for item in out_list:
+            print item[0] # <- field grouped on
+            for foo in item[1]:
+                print foo
+
+        '''
+        AS OF NOV. 2016 (AND THE NEW OR S.O.S. AS SOLE SOURCE), THE DATA STRUCTURE IS MORE LIKE THIS ...
+
+        [300019191,
+        [(300019191, 'United States President and Vice President', 'REP', 300027550, 'Donald J Trump / Mike Pence', '10/28/2016 6:53:36 PM', 1, 149, 0.0909090909090909, '837/1311')
+        (300019191, 'United States President and Vice President', 'DEM', 300027862, 'Hillary Clinton / Tim Kaine', '10/28/2016 6:53:36 PM', 1, 298, 0.181818181818182, '837/1311')
+        (300019191, 'United States President and Vice President', 'PGP', 300027774, 'Jill Stein / Ajamu Baraka', '10/28/2016 6:53:36 PM', 1, 447, 0.272727272727273, '837/1311')
+        (300019191, 'United States President and Vice President', 'LBT', 300027773, 'Gary Johnson / Bill Weld', '10/28/2016 6:53:36 PM', 1, 596, 0.363636363636364, '837/1311')
+        (300019191, 'United States President and Vice President', '', 9901, 'Write-in', '10/28/2016 6:53:36 PM', 1, 149, 0.0909090909090909, '837/1311')]
+        ]
+        '''
+
+        region_list = ['National', 'Oregon', 'Eugene/Springfield', 'Lane County', 'Region']
+        for region in region_list:
+            obj, created = Region.objects.get_or_create(name=region,
+                           defaults={'name': region})
+
+        #
+        # Flush out data from previous grab.
+        #
+        Contest.objects.all().delete()
+        Cand_yes_no.objects.all().delete()
+        Contest_wrapper.objects.all().delete()
+
+        for contest_item in out_list:
+            wrapper, position = ('', '')
+            try:
+    #             wrapper, position = contest_item[0].split(',', 1)
+                wrapper, position = contest_item[1][0][1].split(',', 1)
+                wrapper = wrapper.replace('Commissioner', '').strip()
+                wrapper = wrapper.replace('Director', '').strip()
+                position = position.strip()
+            except ValueError:
+    #             pass
+                wrapper = contest_item[1][0][1].strip()
+                position = ''
+
+            # Fix stuff like "LINN SOIL AND WATER CONSERVATION DISTRICT"
+            if wrapper.isupper():
+                wrapper = wrapper.title()
+
+            new_contest = Contest(
+    # #             name = contest_item[0],
+                name = contest_item[1][0][1],
+                contest_number = contest_item[1][0][0],
+                precincts = contest_item[1][0][9].split('/')[1],
+                precincts_counted  = contest_item[1][0][9].split('/')[0],
+    #             party_code = contest_item[1][0][2], # only if a primary
+    # #             district_category = contest_item[1][0][9],
+                region_id = 4, # Just default to Lane County
+                print_only = True,
+            )
+            new_contest.save()
+            c = new_contest.id
+
+            if wrapper:
+                cw, created = Contest_wrapper.objects.get_or_create(name=wrapper)
+                new_contest.contest_wrapper = cw
+                new_contest.name = position
+                new_contest.save()
+
+            d = Contest.objects.get(pk = c)
+            for contest_details in contest_item[1]:
+                if contest_details[2]:
+                    affiliation = AFFILIATION_LOOKUP.get(contest_details[2], '')
+                else:
+                    affiliation = ''
+                if contest_details[4].isupper():
+                    a_contest_name = contest_details[4].lower()
+                else:
+                    a_contest_name = contest_details[4]
+                d.cand_yes_no_set.create(
+                contest = d,
+                name = a_contest_name,
+                candidate_number = int(contest_details[0]),
+                affiliation = affiliation,
+                incumbent = False,
+                )
