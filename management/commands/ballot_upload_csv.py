0afdcb0459d697a567e2885a270d8fec1e0ba29e
John Heasly <jheasly@earthlink.net>
2016-10-30 19:44:26 -0700

    Now gets, uploads Lane, Statewide, Legislative .csvs

--- management/commands/ballot_upload_csv.py
+++ management/commands/ballot_upload_csv.py
@@ -1,5 +1,5 @@
 from ballot.management.commands.ballot_settings import SITE_ADDR,\
-    CSV_DIRECTORY, CSV_FILE_NAME, SCP_STRING
+    CSV_DIRECTORY, CSV_FILE_NAMES, SCP_STRING
 from django.core.management.base import BaseCommand, CommandError
 
 import os
@@ -17,28 +17,37 @@ class ScrapingBrowser(webdriver.Chrome):
         self.implicitly_wait(3)
         self.get(addr)
 
-    def click_county_download(self, location):
+    def click_sos_download(self, location):
         self.find_element_by_xpath(location).click()
 
 
 class Command(BaseCommand):
     help = '''Simulates a browser JavaScript click required to download .csv
  file from Oregon Secretary of State website. It then uploads file to desired
- server.'''
+ server.
+
+ REQUIRES:
+ * selenium ("pip install selenium")
+ * chromedriver ("https://sites.google.com/a/chromium.org/chromedriver/downloads")
+     on Mac OS X, /usr/local/bin seens a good location for it)
+ * working scp connection to target server'''
 
     def handle(self, *args, **options):
-        # Check to see if there's already a copy of the .csv file downloaded
-        # and delete it, if so. (OS X will auto-increment file name, won't
-        # overwrite it, and we don't want that.)
-        if CSV_FILE_NAME in os.listdir(CSV_DIRECTORY):
-            os.remove('{0}{1}'.format(CSV_DIRECTORY, CSV_FILE_NAME))
-
         # Selenium replicates browser click to download .csv
         browser = ScrapingBrowser(SITE_ADDR)
-        time.sleep(3)
-        browser.click_county_download('/html/body/form/div[4]/div[4]/div[3]/div/div[20]/a')
-        time.sleep(10)
+        time.sleep(.5)
+
+        for csv_file_name in CSV_FILE_NAMES:
+            # Check to see if there's already a copy of the .csv file downloaded
+            # and delete it, if so. (OS X will auto-increment file name, won't
+            # overwrite it, and we don't want that.)
+            if csv_file_name['file'] in os.listdir(CSV_DIRECTORY):
+                os.remove('{0}{1}'.format(CSV_DIRECTORY, csv_file_name['file']))
+
+            browser.click_sos_download(csv_file_name['xpath'])
+            time.sleep(3)
+
+            os.system('scp {0}{1} {2}'.format(CSV_DIRECTORY, csv_file_name['file'], SCP_STRING))
+
         browser.close()
         browser.quit()
-
-        os.system('scp {0}{1} {2}'.format(CSV_DIRECTORY, CSV_FILE_NAME, SCP_STRING))
